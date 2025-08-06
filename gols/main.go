9d385b6265e59bf53f1178abf4410d8da51a6d1f
UnaPepsi <119690535+UnaPepsi@users.noreply.github.com>
2025-08-05 19:38:02 -0600

    more go ls!!!!!!

--- gols/main.go
+++ gols/main.go
@@ -16,12 +16,14 @@ func main() {
 	var colorToUse *string
 	showHidden := slices.Contains(os.Args,"-a")
 	showSize := slices.Contains(os.Args,"-s")
+	showModDate := slices.Contains(os.Args,"-d")
 	var item string
 	entries, err := os.ReadDir("./")
 	if err != nil{
 		log.Fatalf("Couldn't list all items: %v",err.Error())
 	}
 	for _,entry := range entries {
+		item = ""
 		isHidden := fmt.Sprintf("%c",entry.Name()[0]) == "."
 		if isHidden && !showHidden{
 			continue
@@ -39,29 +41,35 @@ func main() {
 				colorToUse = &normal
 			}
 		}
-		item = entry.Name()
-		if showSize{
+		if showSize || showModDate{
 			info, err := entry.Info()
 			if err != nil{
 				log.Fatalf("Couldn't get item info: %v",err.Error())
 			}
-			var size float64
-			if info.IsDir(){ 
-				err = filepath.Walk("./"+info.Name(), func(path string, walkInfo os.FileInfo, err error) error {
+			if showSize{
+				var size float64
+				if info.IsDir(){ 
+					err = filepath.Walk("./"+info.Name(), func(path string, walkInfo os.FileInfo, err error) error {
+						if err != nil{
+							return err
+						}
+						size += float64(walkInfo.Size()) / 1e+6
+						return nil
+					})
 					if err != nil{
-						return err
+						log.Fatalf("Couldn't get item info: %v",err.Error())
 					}
-					size += float64(walkInfo.Size()) / 1e+6
-					return nil
-				})
-				if err != nil{
-					log.Fatalf("Couldn't get item info: %v",err.Error())
+				}else{
+					size = float64(info.Size()) / 1e+6 //MB
 				}
-			}else{
-				size = float64(info.Size()) / 1e+6 //MB
+				item = fmt.Sprintf("%.2fMB - ",size)
 			}
-			item = fmt.Sprintf("%v - %.2fMB",item,size)
+			if showModDate{
+				dateMod := info.ModTime()
+				item = item+fmt.Sprintf("%v - ",dateMod.Format("02/01/2006 15:04"))
+			}
 		}
+		item = item+entry.Name()
 		fmt.Println(*colorToUse+item)
 	}
 	fmt.Print(normal)
